e035cf98fcf7cf49ec9c7a9209004863d40b6722
bela127 <bhb127@outlook.de>
2023-07-20 12:57:36 +0200

    blueprint.py new interface

--- alts/modules/blueprint.py
+++ alts/modules/blueprint.py
@@ -6,25 +6,24 @@ from dataclasses import dataclass, field
 from alts.core.blueprint import Blueprint
 
 from alts.modules.data_process.time_source import IterationTimeSource
-from alts.modules.data_process.time_behavior import NoTimeBehavior
 from alts.modules.data_process.process import DataSourceProcess
 from alts.modules.oracle.query_queue import FCFSQueryQueue
 from alts.modules.oracle.data_source import LineDataSource
 from alts.modules.stopping_criteria import TimeStoppingCriteria
-from alts.modules.data_process.observable_filter import NoObservableFilter
 from alts.modules.queried_data_pool import FlatQueriedDataPool
 from alts.modules.query.query_sampler import LatinHypercubeQuerySampler, UniformQuerySampler
-from alts.core.experiment_modules import ExperimentModules
+from alts.core.experiment_modules import InitQueryExperimentModules
 from alts.core.query.query_selector import ResultQuerySelector
 from alts.modules.query.query_optimizer import NoQueryOptimizer
 from alts.modules.query.query_decider import AllQueryDecider
 from alts.modules.evaluator import PlotNewDataPointsEvaluator, PrintExpTimeEvaluator
+from alts.core.oracle.oracles import POracles
+from alts.core.data.data_pools import ResultDataPools
 
 if TYPE_CHECKING:
     from typing import Iterable, Optional
 
     from alts.core.data_process.time_source import TimeSource
-    from alts.core.data_process.time_behavior import TimeBehavior
     from alts.core.data_process.process import Process
     from alts.core.stopping_criteria import StoppingCriteria
     from alts.core.data_process.observable_filter import ObservableFilter
@@ -33,15 +32,18 @@ if TYPE_CHECKING:
     from alts.core.experiment_modules import ExperimentModules
     from alts.core.evaluator import Evaluator
     from alts.core.oracle.query_queue import QueryQueue
+    from alts.core.oracle.oracles import Oracles
+    from alts.core.data.data_pools import DataPools
 
 @dataclass
 class BaselineBlueprint(Blueprint):
     repeat: int = 1
 
     time_source: TimeSource = IterationTimeSource()
-    time_behavior: TimeBehavior = NoTimeBehavior()
 
-    query_queue: QueryQueue = FCFSQueryQueue()
+    oracles: Oracles = POracles(process = FCFSQueryQueue())
+
+    data_pools: DataPools = ResultDataPools(result=FlatQueriedDataPool())
 
     process: Process = DataSourceProcess(
         data_source=LineDataSource()
@@ -49,16 +51,9 @@ class BaselineBlueprint(Blueprint):
 
     stopping_criteria: StoppingCriteria = TimeStoppingCriteria(stop_time=100)
 
-    observable_stream: ObservableFilter = NoObservableFilter()
-    stream_data_pool: QueriedDataPool = FlatQueriedDataPool()
-    observable_process: ObservableFilter = NoObservableFilter()
-    process_data_pool: QueriedDataPool = FlatQueriedDataPool()
-    observable_results: ObservableFilter = NoObservableFilter()
-    result_data_pool: QueriedDataPool = FlatQueriedDataPool()
 
-    initial_query_sampler: QuerySampler = LatinHypercubeQuerySampler(num_queries=10)
-
-    experiment_modules: ExperimentModules = ExperimentModules(
+    experiment_modules: ExperimentModules = InitQueryExperimentModules(
+        initial_query_sampler = LatinHypercubeQuerySampler(num_queries=10),
         query_selector=ResultQuerySelector(
             query_optimizer=NoQueryOptimizer(query_sampler=UniformQuerySampler()),
             query_decider=AllQueryDecider(),
