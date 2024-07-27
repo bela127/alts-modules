69deb58eb480b05ddd63012d886921afbee4fe7a
Benjamin Gors <benjamin.gorskis@gmail.com>
2024-07-27 12:09:23 +0200

    unit testing

--- tests/oracle/data_source.py
+++ tests/oracle/data_source.py
@@ -8,6 +8,7 @@ from alts.core.configuration import pre_init, is_set, init, post_init, Configura
 
 import numpy as np
 import GPy
+import pytest
 
 if TYPE_CHECKING:
     from typing import Tuple, List, Any, Type
@@ -15,39 +16,18 @@ if TYPE_CHECKING:
 
 """
 Structure: 
-General loop
-General Parameters
-Specific Parameters
-Specific Value Ranges
-General Edgecases
-Specific Edgecases
+One Dimensional Test
+Multidimensional Test
+
 """
 class DataSourceTest(Configurable):
     ds : DataSource = init(default_factory=dsm.RandomUniformDataSource)
 
-    def assign_module(self, pmodule : DataSource):
-        self.ds = pmodule
+    def test(self, values : NDArray[Shape["query_dim, result_dim"], Number]): # type: ignore
+        pass
 
-    def test(self, loops : int = 10000) -> bool:
-        data : list = []
-        result : float
-        rand = ds.RandomUniformDataSource(query_shape = self.ds.query_shape, result_shape = self.ds.result_shape,l = self.ds.query_constrain().ranges[0][0] , u = self.ds.query_constrain().ranges[0][1])()
-        for i in range(loops):
-            a = rand.query(np.asarray((1,)) * float(i)/loops)[0]
-            try:
-                result = self.ds.query(a)[0]
-                if result < self.ds.result_constrain().ranges[0][0] or result >= self.ds.result_constrain().ranges[0][1]:
-                    raise ValueError
-                data.append(result)
-            except ValueError:
-                print("ValueError on query: {} \nWith the result: {} \nWith the settings: {}, {}".format(a, result, self.ds.query_constrain(), self.ds.result_constrain()))
-                return False
-            except Exception as ex:
-                print("{} on query: {} \n With the settings: {}, {}".format(ex, a, self.ds.query_constrain(), self.ds.result_constrain()))
-                return False
-        print()
-        return True
 
 if __name__ == '__main__':
     dst = DataSourceTest()()
     print(dst.ds.default_factory.__annotations__)
+    print(dsm.RandomUniformDataSource.u)
