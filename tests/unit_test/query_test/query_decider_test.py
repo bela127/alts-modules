69057b2da3e2c573e53ec86ce0ce8ef33998b131
Benjamin Gors <benjamin.gorskis@gmail.com>
2024-07-29 15:48:24 +0200

    query_decider unit test 1.0

--- tests/unit_test/query_test/query_decider_test.py
+++ tests/unit_test/query_test/query_decider_test.py
@@ -1,31 +1,48 @@
-from alts.core.oracle.augmentation import Augmentation
+from alts.core.query.query_decider import QueryDecider
 
-def test_candidacy_size():
-    """
-    | **Description**
-    |   To pass, the Decider has to return the right list of preferred candidates 3 times.
-    """
-    ...
+from query_decider_data import *
 
-def test_decide_yes(aug: Augmentation):
+import numpy as np
+
+#TODO Multidimensional testing
+
+TEST_DEPTH = 100
+MAX_DATA_SIZE = 20
+
+def decide_yes(qd: QueryDecider):
     """
     | **Description**
     |   To pass, the Decider has to pick the right candidates 3 times.
     |   Certain Deciders may automatically pass this test, such as ``NoQueryDecider``.
     """
-    ...
+    positive_decisiveness(qd)
 
-def test_decide_no(aug: Augmentation):
+def decide_no(qd: QueryDecider):
     """
     | **Description**
     |   To pass, the Decider has to refuse all given candidates 3 times.
     |   Certain Deciders may automatically pass this test, such as ``AllQueryDecider``.
-
     """
+    negative_decisiveness(qd)
 
-def test_values():
+def normal_values(qd: QueryDecider):
     """
     | **Description**
-    |   To pass, the Decider has to make the right decision 10 times on random inputs.
+    |   To pass, the Decider has to make the right decision on chosen inputs.
     """
-    ...
+    right_decision(qd)
+
+def random_values(qd: QueryDecider):
+    passer = passer_generator(qd)
+    for i in range(TEST_DEPTH):
+        candidates = np.array(list(range(np.random.random_integers(1, MAX_DATA_SIZE))))
+        scores = np.array([[x] for x in ((np.random.random((len(candidates))) - 0.5) * MAX_DATA_SIZE)])
+        assert passer(candidates, scores, qd.decide(candidates, scores))
+
+def test_all():
+    for query_decider in query_deciders:
+        qd = query_decider()
+        decide_yes(qd)
+        decide_no(qd)
+        normal_values(qd)
+        random_values(qd)
