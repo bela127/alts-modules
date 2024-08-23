a2f17239b298c7412997472472056fc90310bc47
Benjamin Gors <benjamin.gorskis@gmail.com>
2024-08-23 14:53:17 +0200

    Initializing unit test for selection_criteria

---
+++ tests/unit_test/query_test/selection_criteria_test.py
@@ -0,0 +1,29 @@
+from alts.core.query.selection_criteria import SelectionCriteria
+import alts.modules.query.selection_criteria as scm
+
+import numpy as np
+import pytest
+
+#List of selection criterias
+selection_criterias = [
+    scm.AllSelectionCriteria,
+    scm.NoSelectionCriteria,
+    scm.RandomSelectionCriteria
+]
+
+#Test for single value
+@pytest.mark.parametrize("sc", selection_criterias)
+def test_single_value(sc: SelectionCriteria):
+    sc = sc()
+    if type(sc) == scm.AllSelectionCriteria:
+        x,y = sc.query(np.array([1]))
+        assert x == np.array([1]) and y == np.ones((1,1))
+    elif type(sc) == scm.NoSelectionCriteria:
+        x,y = sc.query(np.array([1]))
+        assert x == np.array([1]) and y == np.zeros((1,1))
+    elif type(sc) == scm.RandomSelectionCriteria:
+        x,y = sc.query(np.array([1]))
+        assert x == np.array([1]) and y.shape == (1,1) and type(y[0][0]) == float 
+
+#TODO Test random distribution
+#TODO Test multidimensional queries
