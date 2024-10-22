160b94f078d6c01680dd036337e00eb2ddbb43e8
Benjamin Gors <benjamin.gorskis@gmail.com>
2024-10-22 13:30:00 +0200

    Selection Criteria Unit Test 3

--- tests/unit_test/query_test/selection_criteria_test.py
+++ tests/unit_test/query_test/selection_criteria_test.py
@@ -20,7 +20,7 @@ def test_single_value(sc: SelectionCriteria):
         assert np.array_equal(x, np.array([1])) and np.array_equal(y, np.ones((1,1)))
     elif isinstance(sc, scm.NoSelectionCriteria):
         x,y = sc.query(np.array([1]))
-        assert np.array_equal(x, np.array([1])) and np.array_equal(y, np.ones((1,1)))
+        assert np.array_equal(x, np.array([1])) and np.array_equal(y, np.zeros((1,1)))
     elif isinstance(sc, scm.RandomSelectionCriteria):
         x,y = sc.query(np.array([1]))
         assert np.array_equal(x, np.array([1])) and y.shape == (1,1) and isinstance(y.flat[0], float) 
@@ -34,7 +34,7 @@ def test_multiple_queries(sc: SelectionCriteria):
         assert np.array_equal(x, np.array([[0,-1],[2,0],[1,1],[3,2.6],[27,3]])) and np.array_equal(y, np.ones((5,1)))
     elif isinstance(sc, scm.NoSelectionCriteria):
         x,y = sc.query(np.array([[0,-1],[2,0],[1,1],[3,2.6],[27,3]]))
-        assert np.array_equal(x, np.array([[0,-1],[2,0],[1,1],[3,2.6],[27,3]])) and np.array_equal(y, np.ones((5,1)))
+        assert np.array_equal(x, np.array([[0,-1],[2,0],[1,1],[3,2.6],[27,3]])) and np.array_equal(y, np.zeros((5,1)))
     elif isinstance(sc, scm.RandomSelectionCriteria):
         x,y = sc.query(np.array([[0,-1],[2,0],[1,1],[3,2.6],[27,3]]))
         assert np.array_equal(x, np.array([[0,-1],[2,0],[1,1],[3,2.6],[27,3]])) and y.shape == (5,1) and isinstance(y.flat[0], float)
@@ -48,7 +48,7 @@ def test_multiple_dimensions(sc: SelectionCriteria):
         assert np.array_equal(x, np.array([[[2,3],[-1,-1]],[[0,0],[0,0]],[[5.1,3],[1.1,1]],[[-1,-3],[2.6,3.3]],[[1,-5],[3,100]]])) and np.array_equal(y, np.ones((5,1)))
     elif isinstance(sc, scm.NoSelectionCriteria):
         x,y = sc.query(np.array([[[2,3],[-1,-1]],[[0,0],[0,0]],[[5.1,3],[1.1,1]],[[-1,-3],[2.6,3.3]],[[1,-5],[3,100]]]))
-        assert np.array_equal(x, np.array([[[2,3],[-1,-1]],[[0,0],[0,0]],[[5.1,3],[1.1,1]],[[-1,-3],[2.6,3.3]],[[1,-5],[3,100]]])) and np.array_equal(y, np.ones((5,1)))
+        assert np.array_equal(x, np.array([[[2,3],[-1,-1]],[[0,0],[0,0]],[[5.1,3],[1.1,1]],[[-1,-3],[2.6,3.3]],[[1,-5],[3,100]]])) and np.array_equal(y, np.zeros((5,1)))
     elif isinstance(sc, scm.RandomSelectionCriteria):
         x,y = sc.query(np.array([[[2,3],[-1,-1]],[[0,0],[0,0]],[[5.1,3],[1.1,1]],[[-1,-3],[2.6,3.3]],[[1,-5],[3,100]]]))
         assert np.array_equal(x, np.array([[[2,3],[-1,-1]],[[0,0],[0,0]],[[5.1,3],[1.1,1]],[[-1,-3],[2.6,3.3]],[[1,-5],[3,100]]])) and y.shape == (5,1) and isinstance(y.flat[0], float) 
