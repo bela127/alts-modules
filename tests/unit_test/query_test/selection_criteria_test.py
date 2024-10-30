c2adc5ac95d5a5810821047d3685dec9872a9760
Benjamin Gors <benjamin.gorskis@gmail.com>
2024-10-30 17:26:47 +0100

    Finished Selection Criteria Unit Test

--- tests/unit_test/query_test/selection_criteria_test.py
+++ tests/unit_test/query_test/selection_criteria_test.py
@@ -4,6 +4,14 @@ import alts.modules.query.selection_criteria as scm
 import numpy as np
 import pytest
 
+"""
+| **Test aims**
+|   The selection criteria modules are tested for:
+|   - single queries
+|   - multiple queries
+|   - multidimensional queries
+"""
+
 #List of selection criterias
 selection_criterias = [
     scm.AllSelectionCriteria,
@@ -14,6 +22,10 @@ selection_criterias = [
 #Test for single value
 @pytest.mark.parametrize("sc", selection_criterias)
 def test_single_value(sc: SelectionCriteria):
+    """
+    | **Description**
+    |   To pass this test, the module has to correctly select from the single query [1].
+    """
     sc = sc()
     if isinstance(sc, scm.AllSelectionCriteria):
         x,y = sc.query(np.array([1]))
@@ -28,6 +40,10 @@ def test_single_value(sc: SelectionCriteria):
 #Test for multiple values
 @pytest.mark.parametrize("sc", selection_criterias)
 def test_multiple_queries(sc: SelectionCriteria):
+    """
+    | **Description**
+    |   To pass this test, the module has to correctly select from a list of 1-D queries.
+    """
     sc = sc()
     if isinstance(sc, scm.AllSelectionCriteria):
         x,y = sc.query(np.array([[0,-1],[2,0],[1,1],[3,2.6],[27,3]]))
@@ -42,6 +58,10 @@ def test_multiple_queries(sc: SelectionCriteria):
 #Test for multiple dimensions
 @pytest.mark.parametrize("sc", selection_criterias)
 def test_multiple_dimensions(sc: SelectionCriteria):
+    """
+    | **Description**
+    |   To pass this test, the module has to correctly select from a list of 1-D queries.
+    """
     sc = sc()
     if isinstance(sc, scm.AllSelectionCriteria):
         x,y = sc.query(np.array([[[2,3],[-1,-1]],[[0,0],[0,0]],[[5.1,3],[1.1,1]],[[-1,-3],[2.6,3.3]],[[1,-5],[3,100]]]))
