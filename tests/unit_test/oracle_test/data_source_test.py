696fa13281b5db302dbaa826a8a8c8dc916ef5c7
Benjamin Gors <benjamin.gorskis@gmail.com>
2024-11-02 11:32:31 +0100

    Added single/multi query test to datasource unit test

--- tests/unit_test/oracle_test/data_source_test.py
+++ tests/unit_test/oracle_test/data_source_test.py
@@ -2,6 +2,7 @@ from alts.core.oracle.data_source import DataSource
 import alts.modules.oracle.data_source as dsm
 
 import pytest
+import numpy as np
 
 """
 | **Test aims**
@@ -12,6 +13,7 @@ import pytest
 |   - multiple queries
 |   - 1 normal case and all edge cases for parameters
 """
+queries: list[float] = [0,0.1,0.25,0.5,0.75]
 
 #List of DataSources
 data_sources = [
@@ -46,6 +48,10 @@ data_sources = [
 
 #Pass function for single query
 def passer_generator(ds: DataSource, **kwargs):
+    """
+    | **Description**
+    |   Compares with expected behaviour.
+    """
     if type(ds) == dsm.BrownianDriftDataSource:
         raise NotImplementedError()
     elif type(ds) == dsm.BrownianProcessDataSource:
@@ -95,7 +101,8 @@ def passer_generator(ds: DataSource, **kwargs):
     elif type(ds) == dsm.TimeBehaviorDataSource:
         raise NotImplementedError()
     elif type(ds) == dsm.TimeDataSource:
-        raise UserWarning("Correctness depends on correctness of used sub-DataSource")
+        #raise UserWarning("Correctness of TimeDataSource depends on correctness of used sub-DataSource")
+        return lambda x,y: True
     elif type(ds) == dsm.ZDataSource:
         return lambda x,y: True if y in {ds.a*x, -ds.a/2, ds.a/2} else False
     elif type(ds) == dsm.ZInvDataSource:
@@ -103,67 +110,21 @@ def passer_generator(ds: DataSource, **kwargs):
     else:
         raise ValueError("DataSource not found")
 
-#Basic Value test
+#Single Query test
 @pytest.mark.parametrize("ds", data_sources)
-def basic_value_test(ds: DataSource):
+def single_query_test(ds: DataSource):
     ds = ds()
-    if type(ds) == dsm.BrownianDriftDataSource:
-        raise NotImplementedError()
-    elif type(ds) == dsm.BrownianProcessDataSource:
-        raise NotImplementedError()
-    elif type(ds) == dsm.CrossDataSource:
-        return lambda x,y: True if y in {-ds.a*x,ds.a*x}
-    elif type(ds) == dsm.DoubleLinearDataSource:
-        return lambda x,y: True if y in {ds.a*x,ds.s*ds.a*x} else False 
-    elif type(ds) == dsm.ExpDataSource:
-        return lambda x,y: True if y == ds.s*(ds.b**x) else False 
-    elif type(ds) == dsm.GaussianProcessDataSource:
-        raise NotImplementedError()
-    elif type(ds) == dsm.HourglassDataSource:
-        return lambda x,y: True if y in {-ds.a*x, -ds.a*x, -ds.a/2, ds.a/2} else False 
-    elif type(ds) == dsm.HypercubeDataSource:
-        raise NotImplementedError()
-    elif type(ds) == dsm.HyperSphereDataSource:
-        raise NotImplementedError()
-    elif type(ds) == dsm.IndependentDataSource:
-        raise NotImplementedError()
-    elif type(ds) == dsm.InterpolatingDataSource:
-        raise UserWarning("Correctness depends on correctness of used interpolation strategies")
-    elif type(ds) == dsm.LinearPeriodicDataSource:
-        return lambda x,y: True if y == (ds.a*x) % ds.p else False 
-    elif type(ds) == dsm.LinearStepDataSource:
-        return lambda x,y: True if y == ds.a*(x-x%ds.p)/ds.p else False 
-    elif type(ds) == dsm.LineDataSource:
-        return lambda x,y: True if y == ds.a*x + ds.b else False 
-    elif type(ds) == dsm.MixedBrownDriftDataSource:
-        raise NotImplementedError()
-    elif type(ds) == dsm.MixedDriftDataSource:
-        raise NotImplementedError()
-    elif type(ds) == dsm.PowDataSource:
-        return lambda x,y: True if y == ds.s*(x**ds.p) else False 
-    elif type(ds) == dsm.RandomUniformDataSource:
-        return lambda x,y: True if y >= ds.l and y < ds.u else False
-    elif type(ds) == dsm.RBFDriftDataSource:
-        raise NotImplementedError()
-    elif type(ds) == dsm.SinDriftDataSource:
-        raise NotImplementedError()
-    elif type(ds) == dsm.SineDataSource:
-        raise NotImplementedError()
-    elif type(ds) == dsm.SquareDataSource:
-        return lambda x,y: True if y == ds.s * (x - ds.x0)**2 + ds.y0 else False 
-    elif type(ds) == dsm.StarDataSource:
-        raise NotImplementedError()
-    elif type(ds) == dsm.TimeBehaviorDataSource:
-        raise NotImplementedError()
-    elif type(ds) == dsm.TimeDataSource:
-        raise UserWarning("Correctness depends on correctness of used sub-DataSource")
-    elif type(ds) == dsm.ZDataSource:
-        return lambda x,y: True if y in {ds.a*x, -ds.a/2, ds.a/2} else False
-    elif type(ds) == dsm.ZInvDataSource:
-        return lambda x,y: True if y in {-ds.a*x, -ds.a/2, ds.a/2} else False 
-    else:
-        raise ValueError("DataSource not found")
+    f = passer_generator(ds)
+    for query in queries:
+        assert f(query, ds.query(np.array(query)))
 
-
+#Multiple Queries test
+@pytest.mark.parametrize("ds", data_sources)
+def multiple_queries_test(ds: DataSource):
+    ds = ds()
+    f = passer_generator(ds)
+    ds_results = ds.query(np.array([[value] for value in queries]))
+    for query, ds_result in zip(queries, ds_results):
+        assert f(query, ds_result)
 
 
