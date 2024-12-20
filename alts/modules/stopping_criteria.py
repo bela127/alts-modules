a069b730862c464f00ee9e144023375f52eaa711
Benjamin Gors <benjamin.gorskis@gmail.com>
2024-12-20 10:45:30 +0100

    Doku stopping_criteria.py

--- alts/modules/stopping_criteria.py
+++ alts/modules/stopping_criteria.py
@@ -1,3 +1,7 @@
+#Version 1.1.1 conform as of 20.12.2024
+"""
+| *alts.modules.stopping_criteria*
+"""
 from __future__ import annotations
 from typing import TYPE_CHECKING
 
@@ -11,15 +15,48 @@ if TYPE_CHECKING:
 
 @dataclass
 class TimeStoppingCriteria(StoppingCriteria):
+    """
+    TimeStoppingCriteria(exp, stop_time)
+    | **Description**
+    |   This stopping criteria is fulfilled if the internal experiment time exceeds a given ``stop_time``.
+
+    :param exp: The experiment to monitor
+    :type exp: Experiment
+    :param stop_time: The stopping time
+    :type stop_time: float
+    """
     stop_time: float = init()
 
     @property
     def next(self) -> bool:
+        """
+        next(self) -> bool
+        | **Description**
+        |   Checks whether the experiment should stop.
+
+        :return: True if experiment time has reached or exceeded stopping time (else False)
+        :rtype: bool
+        """
         return  self.stop_time >= self.exp.time_source.time
 
 @dataclass
 class DataExhaustedStoppingCriteria(StoppingCriteria):
+    """
+    DataExhaustedStoppingCriteria(exp)
+    | **Description**
+    |
 
+    :param exp: The experiment to monitor
+    :type exp: Experiment
+    """
     @property
     def next(self) -> bool:
+        """
+        next(self) -> bool
+        | **Description**
+        |   Checks whether the experiment should stop.
+
+        :return: True if the experiment's DataSource has been exhausted (else False)
+        :rtype: bool
+        """
         return False #TODO
