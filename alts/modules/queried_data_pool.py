7cf1071df210e8e2305b1b22994c146e966c13cd
Benjamin Gors <benjamin.gorskis@gmail.com>
2025-04-17 13:22:57 +0200

    Doc Queried Data Pool v1.1.1

--- alts/modules/queried_data_pool.py
+++ alts/modules/queried_data_pool.py
@@ -1,3 +1,8 @@
+#Version 1.1.1 conform as of 15.04.2025
+"""
+| *alts.modules.queried_data_pool*
+| :doc:`Core Module </core/data/queried_data_pool>`
+"""
 from __future__ import annotations
 from typing import TYPE_CHECKING
 
@@ -13,15 +18,37 @@ if TYPE_CHECKING:
 
 class FlatQueriedDataPool(QueriedDataPool):
     """
-    implements a pool of already labeled data
+    FlatQueriedDataPool(query_constrain, result_constrain)
+    | **Description**
+    |   This queryable data pool returns a random result matching the query.
+
+    :param query_constrain: The data pool's query constraints
+    :type query_constrain: QueryConstrain
+    :param result_constrain: The data pool's result constraints
+    :type result_constrain: ResultConstrain
     """
 
     def __init__(self):
+        """
+        __init__(self) -> None
+        | **Description**
+        |   Initializes empty lists and dicts.
+        """
         super().init(FlatQueriedDataPool)
         self.query_index: Dict = {}
 
     
     def query(self, queries):
+        """
+        query(self, queries) -> data_points
+        | **Description**
+        |   Returns a list containing a random associated result for each query in queries.
+
+        :param queries: Requested queries
+        :type queries: `NDArray <https://numpy.org/doc/stable/reference/arrays.ndarray.html>`_
+        :return: The tuple of queries and their associated results
+        :rtype: Tuple[`NDArray <https://numpy.org/doc/stable/reference/arrays.ndarray.html>`_,`NDArray <https://numpy.org/doc/stable/reference/arrays.ndarray.html>`_]
+        """
         result_list = []
         for query in queries:
             result_candidate = self.query_index.get(tuple(query), [])
@@ -33,6 +60,14 @@ class FlatQueriedDataPool(QueriedDataPool):
 
 
     def add(self, data_points):
+        """
+        add(self, data_points) -> None
+        | **Description**
+        |   Adds the new result to the query to the already existing results to the same query.
+
+        :param data_points: A tuple of queries and their associated results. 
+        :type data_points: Tuple[`NDArray <https://numpy.org/doc/stable/reference/arrays.ndarray.html>`_,`NDArray <https://numpy.org/doc/stable/reference/arrays.ndarray.html>`_]
+        """
         queries, results = data_points
         for query, result in zip(queries, results):
 
@@ -41,8 +76,24 @@ class FlatQueriedDataPool(QueriedDataPool):
         super().add(data_points)
 
     def query_constrain(self) -> QueryConstrain:
+        """
+        query_constrain(self) -> QueryConstrain
+        | **Description**
+        |   Returns its own query constraints.
+
+        :return: Data pool's query constraints
+        :rtype: QueryConstrain
+        """
         return QueryConstrain(count = self.queries.shape[0], shape = self._query_constrain().shape, ranges = self.queries)
 
 
     def result_constrain(self) -> ResultConstrain:
+        """
+        result_constrain(self) -> ResultConstrain
+        | **Description**
+        |   Returns its own result constraints.
+
+        :return: Data pool's result constraints
+        :rtype: ResultConstrain
+        """
         return self._result_constrain()
