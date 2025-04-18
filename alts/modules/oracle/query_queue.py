d4125bf9b0f3283f7b7f0da0ecaab84111102026
Benjamin Gors <benjamin.gorskis@gmail.com>
2025-04-18 15:36:35 +0200

    Doc v1.1.1

--- alts/modules/oracle/query_queue.py
+++ alts/modules/oracle/query_queue.py
@@ -1,7 +1,16 @@
+#Version 1.1.1 conform as of 18.04.2025
+"""
+| *alts.modules.oracle.query_queue*
+| :doc:`Core Module </core/oracle/query_queue>`
+"""
 from dataclasses import dataclass
 from alts.core.oracle.query_queue import QueryQueue
 
 @dataclass
 class FCFSQueryQueue(QueryQueue):
-    #QUESTION FCFS
+    """
+    FCFSQueryQueue()
+    | **Description**
+    |   The First Come First Serve Query Queue works like a queue, the first added queries are the first ones to be popped!
+    """
     pass
