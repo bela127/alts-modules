1271217d7af8505e9ebe217614c0ef043580f3e8
Benjamin Gors <benjamin.gorskis@gmail.com>
2025-04-23 10:24:59 +0200

    Fully Documented v1.1.1

--- alts/modules/data_process/process.py
+++ alts/modules/data_process/process.py
@@ -1,4 +1,8 @@
-#TODO correct all 
+#Version 1.1.1 conform as of 23.04.2025
+"""
+| *alts.modules.data_process.process*
+| :doc:`Core Module </core/data_process/process>`
+"""
 from __future__ import annotations
 from typing import TYPE_CHECKING, Optional
 
@@ -218,18 +222,17 @@ class DelayedProcess(Process, DelayedConstrained):
             self.oracles.process = self.oracles.process(query_constrain=self.query_constrain)
         else:
             raise TypeError(f"DataSourceProcess requires POracles")
-
-    
-    def step(self, iteration): #TODO
+ 
+    def step(self, iteration):
         """
         step(self, iteration) -> Tuple[data_points, delayed_data_points]
         | **Description**
-        |
+        |   Processes the next queries in the queue. Returns their intermediate and delayed results.
 
-        :param iteration:
-        :type iteration:
-        :return: 
-        :rtype:
+        :param iteration: Unused
+        :type iteration: Any
+        :return: Intermediate queries, their results, Delayed queries, their results
+        :rtype: Tuple[Tuple[`NDArray <https://numpy.org/doc/stable/reference/arrays.ndarray.html>`_,`NDArray <https://numpy.org/doc/stable/reference/arrays.ndarray.html>`_],Tuple[`NDArray <https://numpy.org/doc/stable/reference/arrays.ndarray.html>`_,`NDArray <https://numpy.org/doc/stable/reference/arrays.ndarray.html>`_]]
         """
         queries, results = self.add_intermediate_results()
 
@@ -242,12 +245,10 @@ class DelayedProcess(Process, DelayedConstrained):
         """
         add_intermediate_results(self) -> data_points
         | **Description**
-        |
+        |   Processes the next queries and returns the intermediate results.
 
-        :param :
-        :type :
-        :return:
-        :rtype:
+        :return: Processed intermediate queries and their associated results
+        :rtype: Tuple[`NDArray <https://numpy.org/doc/stable/reference/arrays.ndarray.html>`_,`NDArray <https://numpy.org/doc/stable/reference/arrays.ndarray.html>`_]
         """
         queries = None
         results = None
@@ -259,6 +260,14 @@ class DelayedProcess(Process, DelayedConstrained):
         return queries, results
 
     def add_results(self):
+        """
+        add_results(self) -> delayed_data_points
+        | **Description**
+        |   If an intermediate query has been made, queries the delayed queries and results and adds them to its DataPools.
+
+        :return: Delayed data points
+        :rtype: Tuple[`NDArray <https://numpy.org/doc/stable/reference/arrays.ndarray.html>`_,`NDArray <https://numpy.org/doc/stable/reference/arrays.ndarray.html>`_]
+        """
         delayed_queries = None
         delayed_results = None
         if self.has_new_data:
@@ -267,8 +276,18 @@ class DelayedProcess(Process, DelayedConstrained):
             self.data_pools.result.add((delayed_queries, delayed_results))
         return delayed_queries, delayed_results
 
-    def query(self, queries: NDArray[Shape["query_nr, ... query_shape"], Number]) -> Tuple[NDArray[Shape["query_nr, ... query_shape"], Number], NDArray[Shape["query_nr, ... result_shape"], Number]]:
-        #Bug? ResultDataPools has no stream attribute
+    def query(self, queries: NDArray[Shape["query_nr, ... query_shape"], Number]) -> Tuple[NDArray[Shape["query_nr, ... query_shape"], Number], NDArray[Shape["query_nr, ... result_shape"], Number]]: # type: ignore
+        #TODO Bug? ResultDataPools has no stream attribute
+        """
+        query(queries) -> data_points
+        | **Description**
+        |   Queries the given queries and returns their data points.
+
+        :param queries: Requested queries
+        :type queries: `NDArray <https://numpy.org/doc/stable/reference/arrays.ndarray.html>`_
+        :return: Queried queries, Queried queries' results
+        :rtype: Tuple[`NDArray <https://numpy.org/doc/stable/reference/arrays.ndarray.html>`_,`NDArray <https://numpy.org/doc/stable/reference/arrays.ndarray.html>`_]
+        """
         times = self.data_pools.stream.last_queries
         vars = self.data_pools.stream.last_results
         actual_queries = np.concatenate((times, vars, queries[:,2:]), axis=1)
@@ -276,11 +295,30 @@ class DelayedProcess(Process, DelayedConstrained):
         return queries, results
     
     def added_intermediate_data(self, queries, results):
+        """
+        added_intermediate_data(self, queries, results) -> None
+        | **Description**
+        |   Updates its last processed queries and results, and sets its status not ready to process new queries.
+
+        :param queries: Last processed intermediate queries
+        :type queries: `NDArray <https://numpy.org/doc/stable/reference/arrays.ndarray.html>`_
+        :param results: Last processed queries' delayed results
+        :type results: `NDArray <https://numpy.org/doc/stable/reference/arrays.ndarray.html>`_
+        """
         self.last_queries = queries
         self.last_results = results
         self.ready = False
 
     def update(self):
+        """
+        update(self) ->  data_points
+        | **Description**
+        |   Queries the last queried queries again. Sets the ```has_new_data``` flag to True and returns the data points.
+
+        :return: Intermediate queries, delayed results
+        :rtype: Tuple[`NDArray <https://numpy.org/doc/stable/reference/arrays.ndarray.html>`_,`NDArray <https://numpy.org/doc/stable/reference/arrays.ndarray.html>`_]
+        """
+        #TODO Bug? ResultDataPools has no stream attribute
         queries = np.concatenate((self.data_pools.stream.last_queries, self.data_pools.stream.last_results, self.last_queries[:, 2:]), axis=1)
         queries, results = self.data_source.query(queries)
         if not self.ready:
@@ -305,10 +343,10 @@ class DelayedProcess(Process, DelayedConstrained):
         """
         delayed_query(self) -> data_points
         | **Description**
-        |
+        |   Returns the time-modified delayed queries with the delayed results.
 
-        :return:
-        :rtype:
+        :return: Delayed queries, delayed results
+        :rtype: Tuple[`NDArray <https://numpy.org/doc/stable/reference/arrays.ndarray.html>`_,`NDArray <https://numpy.org/doc/stable/reference/arrays.ndarray.html>`_]
         """
         queries = np.concatenate((self.last_queries[:,:1] + self.time_source.time_step ,self.last_queries[:,1:]), axis=1) 
         return queries, self.last_results
@@ -348,24 +386,79 @@ class DelayedProcess(Process, DelayedConstrained):
 
 @dataclass
 class DelayedStreamProcess(DelayedProcess, StreamProcess):
+    """
+    DelayedStreamProcess(time_source, data_pools, oracles, data_source, stop_time, time_behavior)
+    | **Description**
+    |   A DelayedProcess for StreamProcesses.
+
+    :param time_source: Source of time
+    :type time_source: :doc:`TimeSource </core/data_process/process>`
+    :param data_pools: A data structure which saves all processed queries and results
+    :type data_pools: :doc:`ResultDataPools </core/data/data_pools>`
+    :param oracles: The interaction point between the Process and the data source.
+    :type oracles: :doc:`Oracles </core/oracle/oracles>`
+    :param data_source: A DataSource containing all data points
+    :type data_source: DataSource
+    :param stop_time: The stopping time of the experiment (default= 1000)
+    :type stop_time: float
+    :param time_behaviour: A DataSource with time-dependent data
+    :type time_behaviour: TimeDataSource
+    """
     pass
 
 @dataclass
 class IntegratingDSProcess(DelayedStreamProcess):
-    #QUESTION what does it do?
+    """
+    IntegratingDSProcess(time_source, data_pools, oracles, data_source, stop_time, time_behavior)
+    | **Description**
+    |   The Integrating Delayed Stream Process integrates its results over a given time period.
+
+    :param time_source: Source of time
+    :type time_source: :doc:`TimeSource </core/data_process/process>`
+    :param data_pools: A data structure which saves all processed queries and results
+    :type data_pools: :doc:`ResultDataPools </core/data/data_pools>`
+    :param oracles: The interaction point between the Process and the data source.
+    :type oracles: :doc:`Oracles </core/oracle/oracles>`
+    :param data_source: A DataSource containing all data points
+    :type data_source: DataSource
+    :param stop_time: The stopping time of the experiment (default= 1000)
+    :type stop_time: float
+    :param time_behaviour: A DataSource with time-dependent data
+    :type time_behaviour: TimeDataSource
+    :param integration_time: What time period to integrate the results over (default= 4)
+    :type integration_time: float
+    """
     integration_time: float = init(default=4)
-    integrated_result: Optional[NDArray[Shape["data_nr, ... output_shape"], Number]] = pre_init(None)
+    integrated_result: Optional[NDArray[Shape["data_nr, ... output_shape"], Number]] = pre_init(None) # type: ignore
     start_time: float = pre_init(0.0)
     end_time: float = pre_init(0.0)
 
-    sliding_window: Optional[NDArray[Shape["data_nr, ... output_shape"], Number]] = pre_init(None)
+    sliding_window: Optional[NDArray[Shape["data_nr, ... output_shape"], Number]] = pre_init(None) # type: ignore
     
     def added_intermediate_data(self, queries, results):
+        """
+        added_intermediate_data(self, queries, results) -> None
+        | **Description**
+        |   Updates its last processed queries and results, sets its status not ready to process new queries, sets start time and sets the integrated result to a zero-matrix.
+
+        :param queries: Last processed intermediate queries
+        :type queries: `NDArray <https://numpy.org/doc/stable/reference/arrays.ndarray.html>`_
+        :param results: Last processed queries' delayed results
+        :type results: `NDArray <https://numpy.org/doc/stable/reference/arrays.ndarray.html>`_
+        """
         super().added_intermediate_data(queries, results)
         self.start_time = queries[-1, 0]
         self.integrated_result = np.zeros_like(results)
 
     def update(self):
+        """
+        update(self) ->  data_points
+        | **Description**
+        |   Queries the last queried queries again. Sets the end time and ```has_new_data``` flag to True if the integration time has passed. Adds the result to the integrated result. Finally, returns the data points.
+
+        :return: Intermediate queries, delayed results
+        :rtype: Tuple[`NDArray <https://numpy.org/doc/stable/reference/arrays.ndarray.html>`_,`NDArray <https://numpy.org/doc/stable/reference/arrays.ndarray.html>`_]
+        """
         queries, results = super().update()
         self.has_new_data = False
 
@@ -380,10 +473,29 @@ class IntegratingDSProcess(DelayedStreamProcess):
         return queries, results
     
     def added_data(self, queries, results):
+        """
+        added_data(self. queries, results) -> None
+        | **Description**
+        |   Informs this process that all new data has been added and unassigns the integrated result.
+
+        :param queries: Unused 
+        :type queries: Any
+        :param results: Unused
+        :type results: Any
+        """
         super().added_data(queries, results)
         self.integrated_result = None
 
-    def delayed_query(self) -> Tuple[NDArray[Shape["data_nr, ... query_shape"], Number], NDArray[Shape["data_nr, ... result_shape"], Number]]:
+    def delayed_query(self) -> Tuple[NDArray[Shape["data_nr, ... query_shape"], Number], NDArray[Shape["data_nr, ... result_shape"], Number]]: # type: ignore
+        """
+        delayed_query(self) -> data_points
+        | **Description**
+        |   Returns the time-modified delayed queries and the unchanged integrated result.
+
+        :return: Delayed queries, integrated results
+        :rtype: Tuple[`NDArray <https://numpy.org/doc/stable/reference/arrays.ndarray.html>`_,`NDArray <https://numpy.org/doc/stable/reference/arrays.ndarray.html>`_]
+        :raises: ValueError if integrated_result is None
+        """
         last_queries, last_results = super().delayed_query()
         integrated_result = is_set(self.integrated_result)
         queries = np.concatenate((last_queries[:,:1] + (self.end_time - self.start_time),last_queries[:,1:]), axis=1) 
@@ -392,20 +504,67 @@ class IntegratingDSProcess(DelayedStreamProcess):
 
 @dataclass
 class WindowDSProcess(DelayedStreamProcess):
+    """
+    WindowDSProcess(time_source, data_pools, oracles, data_source, stop_time, time_behavior)
+    | **Description**
+    |   The Window Delayed Stream Process integrates the results over a given window size.
+    |   Similar to IntegratingDSProcess, where a new window is opened and filled, except here the window slides over with each new query, thus always being filled.
 
+    :param time_source: Source of time
+    :type time_source: :doc:`TimeSource </core/data_process/process>`
+    :param data_pools: A data structure which saves all processed queries and results
+    :type data_pools: :doc:`ResultDataPools </core/data/data_pools>`
+    :param oracles: The interaction point between the Process and the data source.
+    :type oracles: :doc:`Oracles </core/oracle/oracles>`
+    :param data_source: A DataSource containing all data points
+    :type data_source: DataSource
+    :param stop_time: The stopping time of the experiment (default= 1000)
+    :type stop_time: float
+    :param time_behaviour: A DataSource with time-dependent data
+    :type time_behaviour: TimeDataSource
+    :param window_size: The size of the integrating window (default= 4)
+    :type window_size: float
+    """
     window_size: float = init(default=4)
     
     def post_init(self):
+        """
+        post_init(self) -> None
+        | **Description**
+        |   Initializes its DataSource, POracles, :doc:`ResultDataPools </core/data/data_pools>` and sliding windows. 
+        
+        :raises TypeError: If the DataPools is not a ResultDataPools or the oracles is not POracles
+        """
         super().post_init()
         self.sliding_query_window = np.empty((0, *self.data_source.query_constrain().shape))
         self.sliding_result_window = np.empty((0, *self.data_source.result_constrain().shape))
     
     def added_intermediate_data(self, queries, results):
+        """
+        added_intermediate_data(self, queries, results) -> None
+        | **Description**
+        |   Updates its last processed queries and results, and sets its status not ready to process new queries.
+        |   Also removes the first query of the sliding query window.
+
+        :param queries: Last processed intermediate queries
+        :type queries: `NDArray <https://numpy.org/doc/stable/reference/arrays.ndarray.html>`_
+        :param results: Last processed queries' delayed results
+        :type results: `NDArray <https://numpy.org/doc/stable/reference/arrays.ndarray.html>`_
+        """
         super().added_intermediate_data(queries, results)
         if not self.sliding_query_window.shape[0] == 0:
             queries = self.sliding_query_window[:1]
     
     def update(self):
+        """
+        update(self) ->  data_points
+        | **Description**
+        |   Queries the last queried queries again. Sets the ```has_new_data``` flag to True and returns the data points.
+        |   Also updates the sliding query and result windows.
+
+        :return: Intermediate queries, delayed results
+        :rtype: Tuple[`NDArray <https://numpy.org/doc/stable/reference/arrays.ndarray.html>`_,`NDArray <https://numpy.org/doc/stable/reference/arrays.ndarray.html>`_]
+        """
         queries, results = super().update()
 
         self.sliding_query_window = np.concatenate((self.sliding_query_window, queries))
@@ -419,7 +578,15 @@ class WindowDSProcess(DelayedStreamProcess):
 
         return queries, results
 
-    def delayed_query(self) -> Tuple[NDArray[Shape["data_nr, ... query_shape"], Number], NDArray[Shape["data_nr, ... result_shape"], Number]]:
+    def delayed_query(self) -> Tuple[NDArray[Shape["data_nr, ... query_shape"], Number], NDArray[Shape["data_nr, ... result_shape"], Number]]: # type: ignore
+        """
+        delayed_query(self) -> data_points
+        | **Description**
+        |   Returns the time-modified delayed queries with the sum of the sliding result window.
+
+        :return: Delayed queries, Sliding result window sum
+        :rtype: Tuple[`NDArray <https://numpy.org/doc/stable/reference/arrays.ndarray.html>`_,`NDArray <https://numpy.org/doc/stable/reference/arrays.ndarray.html>`_]
+        """
         last_queries, last_results = super().delayed_query()
         results = np.sum(self.sliding_result_window, axis=0)[None,...]
         return last_queries, results
