1fd9102920c4dc280c6c14448ccb5f2d670902f5
Benjamin Gors <benjamin.gorskis@gmail.com>
2025-06-24 14:37:46 +0200

    Exchanged auto pass for auto skip

--- tests/unit_test/oracle_test/data_source_test.py
+++ tests/unit_test/oracle_test/data_source_test.py
@@ -1,3 +1,4 @@
+
 from alts.core.oracle.data_source import DataSource
 import alts.modules.oracle.data_source as dsm
 
@@ -53,9 +54,9 @@ def passer_generator(ds: DataSource, **kwargs):
     |   Compares with expected behaviour.
     """
     if type(ds) == dsm.BrownianDriftDataSource:
-        raise NotImplementedError()
+        pytest.skip("Not yet implemented")
     elif type(ds) == dsm.BrownianProcessDataSource:
-        raise NotImplementedError()
+        pytest.skip("Not yet implemented")
     elif type(ds) == dsm.CrossDataSource:
         return lambda x,y: True if y in {-ds.a*x,ds.a*x} else False 
     elif type(ds) == dsm.DoubleLinearDataSource:
@@ -63,15 +64,15 @@ def passer_generator(ds: DataSource, **kwargs):
     elif type(ds) == dsm.ExpDataSource:
         return lambda x,y: True if y == ds.s*(ds.b**x) else False 
     elif type(ds) == dsm.GaussianProcessDataSource:
-        raise NotImplementedError()
+        pytest.skip("Not yet implemented")
     elif type(ds) == dsm.HourglassDataSource:
         return lambda x,y: True if y in {-ds.a*x, -ds.a*x, -ds.a/2, ds.a/2} else False 
     elif type(ds) == dsm.HypercubeDataSource:
-        raise NotImplementedError()
+        pytest.skip("Not yet implemented")
     elif type(ds) == dsm.HyperSphereDataSource:
-        raise NotImplementedError()
+        pytest.skip("Not yet implemented")
     elif type(ds) == dsm.IndependentDataSource:
-        raise NotImplementedError()
+        pytest.skip("Not yet implemented")
     elif type(ds) == dsm.InterpolatingDataSource:
         raise UserWarning("Correctness depends on correctness of used interpolation strategies")
     elif type(ds) == dsm.LinearPeriodicDataSource:
@@ -81,25 +82,25 @@ def passer_generator(ds: DataSource, **kwargs):
     elif type(ds) == dsm.LineDataSource:
         return lambda x,y: True if y == ds.a*x + ds.b else False 
     elif type(ds) == dsm.MixedBrownDriftDataSource:
-        raise NotImplementedError()
+        pytest.skip("Not yet implemented")
     elif type(ds) == dsm.MixedDriftDataSource:
-        raise NotImplementedError()
+        pytest.skip("Not yet implemented")
     elif type(ds) == dsm.PowDataSource:
         return lambda x,y: True if y == ds.s*(x**ds.p) else False 
     elif type(ds) == dsm.RandomUniformDataSource:
         return lambda x,y: True if y >= ds.l and y < ds.u else False
     elif type(ds) == dsm.RBFDriftDataSource:
-        raise NotImplementedError()
+        pytest.skip("Not yet implemented")
     elif type(ds) == dsm.SinDriftDataSource:
-        raise NotImplementedError()
+        pytest.skip("Not yet implemented")
     elif type(ds) == dsm.SineDataSource:
-        raise NotImplementedError()
+        pytest.skip("Not yet implemented")
     elif type(ds) == dsm.SquareDataSource:
         return lambda x,y: True if y == ds.s * (x - ds.x0)**2 + ds.y0 else False 
     elif type(ds) == dsm.StarDataSource:
-        raise NotImplementedError()
+        pytest.skip("Not yet implemented")
     elif type(ds) == dsm.TimeBehaviorDataSource:
-        raise NotImplementedError()
+        pytest.skip("Not yet implemented")
     elif type(ds) == dsm.TimeDataSource:
         #raise UserWarning("Correctness of TimeDataSource depends on correctness of used sub-DataSource")
         return lambda x,y: True
@@ -112,7 +113,7 @@ def passer_generator(ds: DataSource, **kwargs):
 
 #Single Query test
 @pytest.mark.parametrize("ds", data_sources)
-def single_query_test(ds: DataSource):
+def test_single_query(ds: DataSource):
     ds = ds()
     f = passer_generator(ds)
     for query in queries:
@@ -120,11 +121,11 @@ def single_query_test(ds: DataSource):
 
 #Multiple Queries test
 @pytest.mark.parametrize("ds", data_sources)
-def multiple_queries_test(ds: DataSource):
+def test_multiple_queries(ds: DataSource):
     ds = ds()
     f = passer_generator(ds)
     ds_results = ds.query(np.array([[value] for value in queries]))
-    for query, ds_result in zip(queries, ds_results):
-        assert f(query, ds_result)
+    for query, ds_result in zip(ds_results[0], ds_results[1]):
+        assert f(query[0], ds_result[0])
 
 
