bbf89f4ab32d556e204e02b98b91f545c2781624
Benjamin Gors <benjamin.gorskis@gmail.com>
2025-07-18 14:31:08 +0200

    Updated QueryDecider to 1.2

--- tests/unit_test/query_test/query_decider_test.py
+++ tests/unit_test/query_test/query_decider_test.py
@@ -1,4 +1,4 @@
-#Test version 1.1 as of 24.06.2025
+#Test version 1.2 as of 18.07.2025
 from alts.core.query.query_decider import QueryDecider
 import alts.modules.query.query_decider as qdm
 
@@ -72,83 +72,53 @@ def test_normal_values(qd: QueryDecider):
     if isinstance(qd, qdm.AllQueryDecider):
         #Normal Queries
         x,y = qd.decide(np.array([0, 1, 2]), np.array([[-1], [0], [0.5]]))
-        assert x == True and np.array_equal(np.sort(y.flat), np.sort(np.array([0, 1, 2]).flat))
+        assert x == True and np.array_equal(np.sort(y.flat), np.sort(np.array([0, 1, 2]).flat)), "All query candidates should be chosen"
         #Consistent returns
         x,y = qd.decide(np.array([0, 1, 2]), np.array([[-1], [0], [0.5]]))
-        assert x == True and np.array_equal(np.sort(y.flat), np.sort(np.array([0, 1, 2]).flat))
+        assert x == True and np.array_equal(np.sort(y.flat), np.sort(np.array([0, 1, 2]).flat)), "All query candidates should be chosen"
         #Similar Scores
         x,y  = qd.decide(np.array([0, 1, 2]), np.array([[0], [0], [0]]))
-        assert x == True and np.array_equal(np.sort(y.flat), np.sort(np.array([0, 1, 2]).flat))
+        assert x == True and np.array_equal(np.sort(y.flat), np.sort(np.array([0, 1, 2]).flat)), "All query candidates should be chosen"
         #Single Candidate
         x,y = qd.decide(np.array([0]), np.array([[0]]))
-        assert x == True and np.array_equal(np.sort(y.flat), np.sort(np.array([0]).flat))
+        assert x == True and np.array_equal(np.sort(y.flat), np.sort(np.array([0]).flat)), "All query candidates should be chosen"
     elif isinstance(qd, qdm.NoQueryDecider):
         #Normal Query
         x,y = qd.decide(np.array([0, 1, 2]), np.array([[-1], [0], [0.5]]))
-        assert x == False and np.array_equal(np.sort(y.flat), np.sort(np.array([]).flat))
+        assert x == False and np.array_equal(np.sort(y.flat), np.sort(np.array([]).flat)), "No query candidates should be chosen"
         #Consistent retutns
         x,y = qd.decide(np.array([0, 1, 2]), np.array([[-1], [0], [0.5]]))
-        assert x == False and np.array_equal(np.sort(y.flat), np.sort(np.array([]).flat))
+        assert x == False and np.array_equal(np.sort(y.flat), np.sort(np.array([]).flat)), "No query candidates should be chosen"
         #Similar Scores
         x,y = qd.decide(np.array([0, 1, 2]), np.array([[0], [0], [0]]))
-        assert x == False and np.array_equal(np.sort(y.flat), np.sort(np.array([]).flat))
+        assert x == False and np.array_equal(np.sort(y.flat), np.sort(np.array([]).flat)), "No query candidates should be chosen"
         #Single Candidate
         x,y = qd.decide(np.array([0]), np.array([[0]]))
-        assert x == False and np.array_equal(np.sort(y.flat), np.sort(np.array([]).flat))
+        assert x == False and np.array_equal(np.sort(y.flat), np.sort(np.array([]).flat)), "No query candidates should be chosen"
     elif isinstance(qd, qdm.ThresholdQueryDecider):
         #Normal Query
         x,y = qd.decide(np.array([0, 1, 2]), np.array([[qd.threshold], [qd.threshold - 0.1], [qd.threshold + 0.1]]))
-        assert x == True and np.array_equal(np.sort(y.flat), np.sort(np.array([2]).flat))
+        assert x == True and np.array_equal(np.sort(y.flat), np.sort(np.array([2]).flat)), "Only queries with scores above the threshold value should be chosen"
         #Consistent returns
         x,y = qd.decide(np.array([0, 1, 2]), np.array([[qd.threshold], [qd.threshold - 0.1], [qd.threshold + 0.1]]))
-        assert x == True and np.array_equal(np.sort(y.flat), np.sort(np.array([2]).flat))
+        assert x == True and np.array_equal(np.sort(y.flat), np.sort(np.array([2]).flat)), "Only queries with scores above the threshold value should be chosen"
     elif isinstance(qd, qdm.TopKQueryDecider):
         #Normal Query
         x,y = qd.decide(np.array([i for i in range(qd.k + 2)]), np.array([[i/2 - 1] for i in range(qd.k + 2)]))
-        assert x == True and np.array_equal(np.sort(y.flat), np.sort(np.array([i for i in range(2, qd.k + 2)]).flat))
+        assert x == True and np.array_equal(np.sort(y.flat), np.sort(np.array([i for i in range(2, qd.k + 2)]).flat)), "Only k query candidates should be chosen"
         #Consistent Returns
         x,y = qd.decide(np.array([i for i in range(qd.k + 2)]), np.array([[i/2 - 1] for i in range(qd.k + 2)]))
-        assert x == True and np.array_equal(np.sort(y.flat), np.sort(np.array([i for i in range(2, qd.k + 2)]).flat))
+        assert x == True and np.array_equal(np.sort(y.flat), np.sort(np.array([i for i in range(2, qd.k + 2)]).flat)), "Only k query candidates should be chosen"
         #Similar Scores
-        assert len(qd.decide(np.array([i for i in range(qd.k + 2)]), np.array([[0] for i in range(qd.k + 2)]))[1].tolist()) == 4
+        assert len(qd.decide(np.array([i for i in range(qd.k + 2)]), np.array([[0] for i in range(qd.k + 2)]))[1].tolist()) == 4, "Only k query candidates should be chosen"
         queries = []
         for query in qd.decide(np.array([i for i in range(qd.k + 2)]), np.array([[0] for i in range(qd.k + 2)]))[1]:
             queries.append(query[0])
-        assert len(set(queries)) == 4
+        assert len(set(queries)) == 4 , "Only k query candidates should be chosen"
         #Less than default candidates
         x,y = qd.decide(np.array([0]), np.array([[0]]))
-        assert x == True and np.array_equal(np.sort(y.flat), np.sort(np.array([0]).flat))
+        assert x == True and np.array_equal(np.sort(y.flat), np.sort(np.array([0]).flat)), "All candidates should be chosen"
         x,y = qd.decide(np.array([0, 1]), np.array([[0],[1]]))
-        assert x == True and np.array_equal(np.sort(y.flat), np.sort(np.array([0, 1]).flat))
-    else:
-        raise ValueError("QueryDecider not found: {}".format(qd))
-
-#Returns the associated pass function
-def passer_generator(qd: QueryDecider):
-    if isinstance(qd, qdm.AllQueryDecider):
-        return lambda candidates, scores, output: True if (output[0], set(output[1])) == (True, set(candidates)) else False
-    elif isinstance(qd, qdm.NoQueryDecider):
-        return lambda candidates, scores, output: True if (output[0], set(output[1])) == (False, set([])) else False
-    elif isinstance(qd, qdm.ThresholdQueryDecider):
-        def f(candidates, scores, output):  # type: ignore
-            sol = set()
-            for i in range(len(candidates)):
-                if scores[i][0] > qd.threshold:
-                    sol.add(candidates[i])
-            if output[0] == bool(set(output[1])) and set(output[1]) == sol:
-                return True
-            else:
-                return False
-        return f
-    elif isinstance(qd, qdm.TopKQueryDecider):
-        def f(candidates, scores, output): #type:ignore
-            least_score = min([scores[candidates.tolist().index(x)][0] for x in output[1]])
-            if len(output[1]) < qd.k and len(output[1]) != len(candidates) or len(output[1]) > qd.k or output[0] == False:
-                return False
-            for i in range(len(candidates)):
-                if candidates[i] not in output[1] and scores[i][0] > least_score:
-                    return False
-            return True
-        return f
+        assert x == True and np.array_equal(np.sort(y.flat), np.sort(np.array([0, 1]).flat)), "All candidates should be chosen"
     else:
         raise ValueError("QueryDecider not found: {}".format(qd))
