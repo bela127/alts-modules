429e4a8f41ebda6d1c12bdf9cd1fc035c325e20d
Benjamin Gors <benjamin.gorskis@gmail.com>
2025-07-18 14:26:19 +0200

    Started QuerySampler testing

--- tests/unit_test/query_test/query_sampler_test.py
+++ tests/unit_test/query_test/query_sampler_test.py
@@ -1,5 +1,16 @@
 from alts.core.query.query_sampler import QuerySampler
+from alts.modules.query.query_optimizer import NoQueryOptimizer
 import alts.modules.query.query_sampler as qss
+
+from alts.core.evaluator import Evaluator, Evaluate
+from alts.core.experiment import Experiment
+from alts.modules.blueprint import BaselineBlueprint
+from alts.core.experiment_modules import InitQueryExperimentModules
+from alts.modules.query.query_sampler import LatinHypercubeQuerySampler, UniformQuerySampler
+from alts.core.query.query_selector import ResultQuerySelector
+from alts.modules.query.query_decider import AllQueryDecider
+from alts.modules.query.selection_criteria import AllSelectionCriteria
+from alts.modules.oracle.data_source import LineDataSource
 
 import numpy as np
 import pytest
@@ -7,8 +18,7 @@ import pytest
 """
 | **Test aims**
 |   The query samplers modules are tested for:
-|   - Correct sampling of data 1D
-|   - Correct sampling of data 2D
+|   - Correct shape of sampled data
 """
 
 query_samplers = [
@@ -33,13 +43,13 @@ query_samplers = [
 ]
 
 queries = np.array([[0],[0.25],[0.5],[0.75],[1]])
-
-@pytest.mark.parametrize("qs", query_samplers)
-def test_1d_sample(qs: QuerySampler):
-    if qs == qss.OptimalQuerySampler:
-        qs = qs(optimal_queries=queries)
-        
     
 @pytest.mark.parametrize("qs", query_samplers)
-def test_2d_sample(qs: QuerySampler):
-    pytest.skip("Not implemented")
+def test_shape(qs: QuerySampler):
+    exp = Experiment(BaselineBlueprint(InitQueryExperimentModules(
+                               initial_query_sampler = LatinHypercubeQuerySampler(num_queries=10),
+                               query_selector=ResultQuerySelector(
+                                   query_optimizer=NoQueryOptimizer(query_sampler=qs()),
+                                   query_decider=AllQueryDecider(),
+                               )
+                           )),exp_nr=1) 
