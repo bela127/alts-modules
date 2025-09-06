afcfda8994b2ab981f3bc2ffee88e40437e17bd0
Benjamin Gors <benjamin.gorskis@gmail.com>
2025-09-06 17:35:37 +0200

    Finished Testing of QueryQueue

--- tests/unit_tests/oracle_test/query_queue_test.py
+++ tests/unit_tests/oracle_test/query_queue_test.py
@@ -1,14 +1,10 @@
-#Test version 1.2 as of 18.07.2025
+#Test version 2.0 as of 06.09.2025
 from alts.core.oracle.query_queue import QueryQueue
 import alts.modules.oracle.query_queue as qq
 
 from alts.modules.data_process.process import DataSourceProcess
 from alts.modules.oracle.data_source import RandomUniformDataSource
 from alts.core.experiment_runner import ExperimentRunner
-from alts.modules.query.query_optimizer import NoQueryOptimizer
-from alts.modules.query.query_sampler import UniformQuerySampler
-from alts.core.query.query_selector import ResultQuerySelector
-from alts.core.experiment_modules import InitQueryExperimentModules
 
 import alts.modules.testing_modules as tm
 import numpy as np
@@ -24,12 +20,13 @@ query_queues = [
     qq.FCFSQueryQueue
 ]
 
-shape_values = [(1,), (1,1)]#, (2,2)]#, (5,3,2), (2,3,4,1)]
+shape_values = [(1,), (1,1), (2,2), (5,3,2), (2,3,4,1)]
 
 @pytest.mark.parametrize("query_shape", shape_values)
 def test_FCFSQueryQueue(query_shape: tuple):
     bp = tm.TestBlueprint(process=DataSourceProcess(data_source=RandomUniformDataSource(query_shape=query_shape)),
-                               evaluators=(tm.QueryEvaluator(func_path="oracles.process.add", query_index=1),))
+                               evaluators=(tm.ConstrainEvaluator(func_path="oracles.process.add", q_index=slice(None,None,None)),
+                                           tm.ConstrainEvaluator(func_path="oracles.process.pop", r_index=slice(None,None,None))))
     er = ExperimentRunner([bp])
     er.run_experiment(bp)
 
