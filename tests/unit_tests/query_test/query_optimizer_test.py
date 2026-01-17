085264ce06163062137a73de08e9d69cf26c2210
Benjamin Gors <benjamin.gorskis@gmail.com>
2026-01-17 17:13:09 +0100

    Changed structure

--- tests/unit_tests/query_test/query_optimizer_test.py
+++ tests/unit_tests/query_test/query_optimizer_test.py
@@ -23,7 +23,8 @@ import pytest
 
 query_optimizers = tm.query_for_members(qos, QueryOptimizer)
 special_query_optimizers = [
-    qos.ProbWeightedMCQueryOptimizer
+    qos.ProbWeightedMCQueryOptimizer,
+    qos.NoQueryOptimizer
 ]
 simple_query_optimizers = np.setdiff1d(query_optimizers, special_query_optimizers, assume_unique=True) # type: ignore
 
@@ -33,14 +34,23 @@ shape_values = [(1,)]#, (1,1)]#, (2,2), (5,3,2), (2,3,4,1)]
 @pytest.mark.parametrize("qo", simple_query_optimizers)
 def test_basic(qo: type[QueryOptimizer], query_shape: tuple):
     bp = tm.TestBlueprint(process=DataSourceProcess(data_source=RandomUniformDataSource(query_shape=query_shape)),
-                          experiment_modules=InitQueryExperimentModules(initial_query_sampler=UniformQuerySampler(num_queries=10), query_selector=ResultQuerySelector(query_optimizer=qo(query_sampler=UniformQuerySampler()), query_decider=AllQueryDecider())),
+                          experiment_modules=InitQueryExperimentModules(initial_query_sampler=UniformQuerySampler(num_queries=10), query_selector=ResultQuerySelector(query_optimizer=qo(), query_decider=AllQueryDecider())),
                           evaluators=(tm.ConstrainEvaluator(func_path="experiment_modules.query_selector.query_optimizer.select", r_index=0),))
     er = ExperimentRunner([bp])
     er.run_experiment(bp)
 
 special_assginments = {
-    qos.ProbWeightedMCQueryOptimizer: None
+    qos.ProbWeightedMCQueryOptimizer: None,
+    qos.NoQueryOptimizer: "test_NoQueryOptimizer"
 }
+
+@pytest.mark.parametrize("query_shape", shape_values)
+def test_NoQueryOptimizer(query_shape: tuple):
+    bp = tm.TestBlueprint(process=DataSourceProcess(data_source=RandomUniformDataSource(query_shape=query_shape)),
+                          experiment_modules=InitQueryExperimentModules(initial_query_sampler=UniformQuerySampler(num_queries=10), query_selector=ResultQuerySelector(query_optimizer=qos.NoQueryOptimizer(query_sampler=UniformQuerySampler()), query_decider=AllQueryDecider())),
+                          evaluators=(tm.ConstrainEvaluator(func_path="experiment_modules.query_selector.query_optimizer.select", r_index=0),))
+    er = ExperimentRunner([bp])
+    er.run_experiment(bp)
 
 @pytest.mark.parametrize("special_query_optimizer", special_query_optimizers)
 def test_special(special_query_optimizer: type[QueryOptimizer]):
