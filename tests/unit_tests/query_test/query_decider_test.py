feef59d949a8a746a079f820296ee804bfe629c9
Benjamin Gors <benjamin.gorskis@gmail.com>
2026-02-07 13:19:48 +0100

    Removed unnecessary test case

--- tests/unit_tests/query_test/query_decider_test.py
+++ tests/unit_tests/query_test/query_decider_test.py
@@ -29,11 +29,10 @@ simple_query_deciders = np.setdiff1d(query_deciders, special_query_deciders, ass
 
 shape_values = [(1,), (1,1), (2,2), (5,3,2), (2,3,4,1)]
 
-k_values = [1,3,10,100,0]
+k_values = [1,3,10,100]
 @pytest.mark.parametrize("k", k_values)
 @pytest.mark.parametrize("query_shape", shape_values)
 def test_topK(k: int, query_shape: tuple):
-    if k == 0: pytest.xfail("Edge case k=0")
     bp = tm.TestBlueprint(process=DataSourceProcess(data_source=RandomUniformDataSource(query_shape=query_shape)),
                                experiment_modules=InitQueryExperimentModules(initial_query_sampler=UniformQuerySampler(num_queries=10), query_selector=ResultQuerySelector(query_optimizer=NoQueryOptimizer(query_sampler=UniformQuerySampler()), query_decider=qdm.TopKQueryDecider(k))),
                                evaluators=(tm.ConstrainEvaluator(func_path="experiment_modules.query_selector.query_decider.decide", q_index=slice(None,None,None), r_index=1),))
