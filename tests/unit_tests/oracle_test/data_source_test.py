9f7a2cdcf5df163e58953035ca967d9576f32dfb
Benjamin Gors <benjamin.gorskis@gmail.com>
2026-02-24 16:53:10 +0100

    Testing

--- tests/unit_tests/oracle_test/data_source_test.py
+++ tests/unit_tests/oracle_test/data_source_test.py
@@ -1,9 +1,9 @@
 import alts.modules.oracle.data_source as dsm
 
 from alts.modules.data_process.process import DataSourceProcess
-from alts.modules.oracle.data_source import LineDataSource
 import alts.modules.blueprint as bps
 from alts.core.experiment_runner import ExperimentRunner
+from alts.core.oracle.data_source import DataSource
 
 import alts.modules.testing_modules as tm
 import pytest
@@ -11,59 +11,72 @@ import numpy as np
 
 """
 | **Test aims**
-|   The data source modules are tested for:
-|   - WIP
+|   The query deciders are tested for:
+|   - Correct output dimensions with respect to paramters
+|   - Handling of nonsensical user parameters
 """
 
-#List of DataSources
-data_sources = [
-    dsm.BrownianDriftDataSource,
-    dsm.BrownianProcessDataSource,
-    dsm.CrossDataSource,
-    dsm.DoubleLinearDataSource,
-    dsm.ExpDataSource,#5
-    dsm.GaussianProcessDataSource,
-    dsm.HourglassDataSource,
-    dsm.HypercubeDataSource,
-    dsm.HyperSphereDataSource,
-    dsm.IndependentDataSource,#10
-    dsm.InterpolatingDataSource,
-    dsm.LinearPeriodicDataSource,
-    dsm.LinearStepDataSource,
-    dsm.LineDataSource,
-    dsm.MixedBrownDriftDataSource,#15
-    dsm.MixedDriftDataSource,
-    dsm.PowDataSource,
-    dsm.RandomUniformDataSource,
-    dsm.RBFDriftDataSource,
-    dsm.SinDriftDataSource,#20
-    dsm.SineDataSource,
-    dsm.SquareDataSource,
-    dsm.StarDataSource,
-    dsm.TimeBehaviorDataSource,
-    dsm.ZDataSource,#25
-    dsm.ZInvDataSource#26
-]
-
-
 shape_values = [(1,1), (2,2)]#, (1,), (5,3,2), (2,3,4,1)]
-parameter_1 = [-2.7,-1]#,0,1,2.7]
+parameter_any = [-2.7,-1]#,0,1,2.7]
+parameter_positve = [1, 2.7]
+parameter_nonpositive = [-2.7, -1]
 
-def query(func, queries):
-    print("hi")
-    results = np.dot(queries, np.ones((1,1))*(-2.7)) + np.ones((1,1))*(-2.7)
-    print(queries.shape)
-    print(np.ones((*(1,1),*(1,1))).shape)
-    print(np.dot(queries, np.ones((*(1,1),*(1,1)))*(-2.7)).shape)
-    print(np.ones((1,1)).shape)
-    return queries, results
 
-@pytest.mark.parametrize("a", parameter_1)
-@pytest.mark.parametrize("b", parameter_1)
+@pytest.mark.parametrize("a", parameter_any)
+@pytest.mark.parametrize("b", parameter_any)
 @pytest.mark.parametrize("result_shape", shape_values)
 @pytest.mark.parametrize("query_shape,", shape_values)
+@pytest.mark.xfail(reason="Fix bad result shapes")
 def test_LineDataSource(query_shape, result_shape, a, b):
-    bp = bps.BaselineBlueprint(process=DataSourceProcess(data_source=LineDataSource(query_shape=query_shape, result_shape=result_shape, a=a, b=b)),
+    bp = bps.BaselineBlueprint(process=DataSourceProcess(data_source=dsm.LineDataSource(query_shape=query_shape, result_shape=result_shape, a=a, b=b)),
                                evaluators=(tm.ConstraintEvaluator(func_path="process.data_source.query", q_index=slice(None,None,None), r_index=1),))
     er = ExperimentRunner([bp])
     er.run_experiment(bp)
+
+@pytest.mark.parametrize("u", parameter_nonpositive)
+@pytest.mark.parametrize("l", parameter_any)
+@pytest.mark.parametrize("result_shape", shape_values)
+@pytest.mark.parametrize("query_shape,", shape_values)
+@pytest.mark.xfail()
+def test_RandomUniformDataSource(query_shape, result_shape, u, l):
+    bp = bps.BaselineBlueprint(process=DataSourceProcess(data_source=dsm.RandomUniformDataSource(query_shape=query_shape, result_shape=result_shape, u=u+l, l=l)),
+                               evaluators=(tm.ConstraintEvaluator(func_path="process.data_source.query", q_index=slice(None,None,None), r_index=1),))
+    er = ExperimentRunner([bp])
+    er.run_experiment(bp)
+
+
+data_sources = tm.query_for_members(dsm, DataSource)
+
+#List of DataSources
+tested_data_sources = {
+    dsm.BrownianDriftDataSource : None,
+    dsm.BrownianProcessDataSource : None,
+    dsm.CrossDataSource : None,
+    dsm.DoubleLinearDataSource : None,
+    dsm.ExpDataSource : None,#5
+    dsm.GaussianProcessDataSource : None,
+    dsm.HourglassDataSource : None,
+    dsm.HypercubeDataSource : None,
+    dsm.HyperSphereDataSource : None,
+    dsm.IndependentDataSource : None,#10
+    dsm.InterpolatingDataSource : None,
+    dsm.LinearPeriodicDataSource : None,
+    dsm.LinearStepDataSource : None,
+    dsm.LineDataSource : test_LineDataSource,
+    dsm.MixedBrownDriftDataSource : None,#15
+    dsm.MixedDriftDataSource : None,
+    dsm.PowDataSource : None,
+    dsm.RandomUniformDataSource : None,
+    dsm.RBFDriftDataSource : None,
+    dsm.SinDriftDataSource : None,#20
+    dsm.SineDataSource : None,
+    dsm.SquareDataSource : None,
+    dsm.StarDataSource : None,
+    dsm.TimeBehaviorDataSource : None,
+    dsm.ZDataSource : None,#25
+    dsm.ZInvDataSource : None#26
+}
+
+@pytest.mark.parametrize("untested_data_source", np.setdiff1d(data_sources, list(tested_data_sources.keys()), assume_unique=True))
+def test_special_tested(untested_data_source: type[DataSource]):
+    pytest.xfail(f"DataSource {untested_data_source.__name__} not tested")
--- tests/unit_tests/query_test/query_sampler_test.py
+++ tests/unit_tests/query_test/query_sampler_test.py
@@ -2,54 +2,51 @@ from alts.core.query.query_sampler import QuerySampler
 from alts.modules.query.query_optimizer import NoQueryOptimizer
 import alts.modules.query.query_sampler as qss
 
-from alts.core.evaluator import Evaluator, Evaluate
-from alts.core.experiment import Experiment
-from alts.modules.blueprint import BaselineBlueprint
-from alts.core.experiment_modules import InitQueryExperimentModules
-from alts.modules.query.query_sampler import LatinHypercubeQuerySampler, UniformQuerySampler
-from alts.core.query.query_selector import ResultQuerySelector
+from alts.modules.data_process.process import DataSourceProcess
+from alts.modules.oracle.data_source import RandomUniformDataSource
+from alts.core.experiment_runner import ExperimentRunner
 from alts.modules.query.query_decider import AllQueryDecider
-from alts.modules.query.selection_criteria import AllSelectionCriteria
-from alts.modules.oracle.data_source import LineDataSource
+from alts.modules.query.query_optimizer import NoQueryOptimizer
+from alts.modules.query.query_sampler import UniformQuerySampler
+from alts.core.query.query_selector import ResultQuerySelector
+from alts.core.experiment_modules import InitQueryExperimentModules
 
+import alts.modules.testing_modules as tm
 import numpy as np
 import pytest
 
 """
 | **Test aims**
-|   The query samplers modules are tested for:
-|   - Correct shape of sampled data
+|   The query samplers are tested for:
+|   - Correct output dimensions with respect to paramters
+|   - Handling of nonsensical user parameters
 """
+query_samplers = tm.query_for_members(qss, QuerySampler)
+special_query_samplers = [
 
-query_samplers = [
-    qss.OptimalQuerySampler,
-    qss.FixedQuerySampler,
-    qss.FixedQuerySampler,
-    qss.UniformQuerySampler,
-    qss.LatinHypercubeQuerySampler,
-    qss.RandomChoiceQuerySampler,
-    qss.ProcessQuerySampler,
-    qss.LastProcessQuerySampler,
-    qss.ProcessQueueQuerySampler,
-    qss.DataPoolQuerySampler,
-    qss.AllDataPoolQuerySampler,
-    qss.AllResultPoolQuerySampler,
-    qss.AllStreamPoolQuerySampler,
-    qss.AllProcessPoolQuerySampler,
-    qss.LastDataPoolQuerySampler,
-    qss.LastResultPoolQuerySampler,
-    qss.LastStreamPoolQuerySampler,
-    qss.LastProcessPoolQuerySampler
 ]
+simple_query_samplers = np.setdiff1d(query_samplers, special_query_samplers, assume_unique=True)
+
 
 queries = np.array([[0],[0.25],[0.5],[0.75],[1]])
+shape_values = [(1,), (1,1), (2,2), (5,3,2), (2,3,4,1)]
     
-@pytest.mark.parametrize("qs", query_samplers)
-def test_shape(qs: QuerySampler):
-    exp = Experiment(BaselineBlueprint(InitQueryExperimentModules(
-                               initial_query_sampler = LatinHypercubeQuerySampler(num_queries=10),
-                               query_selector=ResultQuerySelector(
-                                   query_optimizer=NoQueryOptimizer(query_sampler=qs()),
-                                   query_decider=AllQueryDecider(),
-                               )
-                           )),exp_nr=1) 
+@pytest.mark.parametrize("query_shape", shape_values)
+@pytest.mark.parametrize("qs", simple_query_samplers)
+def test_QuerySampler(qs: QuerySampler, query_shape):
+    bp = tm.TestBlueprint(process=DataSourceProcess(data_source=RandomUniformDataSource(query_shape=query_shape)),
+                               experiment_modules=InitQueryExperimentModules(initial_query_sampler=UniformQuerySampler(num_queries=10), query_selector=ResultQuerySelector(query_optimizer=NoQueryOptimizer(query_sampler=qs()), query_decider=AllQueryDecider())),
+                               evaluators=(tm.ConstraintEvaluator(func_path="experiment_modules.query_selector.query_optimizer.query_sampler.sample", q_index=None, r_index=slice(None,None,None)),))
+    er = ExperimentRunner([bp])
+    er.run_experiment(bp)
+    
+special_assginments = {
+
+}
+
+@pytest.mark.parametrize("special_query_sampler", special_query_samplers)
+def test_special_tested(special_query_sampler: type[QuerySampler]):
+    if (special_query_sampler not in special_assginments):
+        pytest.xfail(f"Special declared QuerySampler {special_query_sampler.__name__} not tested")
+    if special_assginments[special_query_sampler] is None:
+        pytest.xfail(f"Not yet implemented: {special_query_sampler.__name__}")
